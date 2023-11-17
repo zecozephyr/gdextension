7a8f2421d499d0f8393b0e645a7dcf4ff0e8736e
j <>
2023-11-17 12:05:34 -0800

    extra make web

--- examples/dodge-the-creeps/rust/build-wasm.sh
+++ examples/dodge-the-creeps/rust/build-wasm.sh
@@ -9,4 +9,18 @@ cd `dirname "$0"`
 
 # We build the host gdextension first so that the godot editor doesn't complain.
 cargo +nightly build --package dodge-the-creeps &&
-cargo +nightly build --package dodge-the-creeps --target wasm32-unknown-emscripten -Zbuild-std $@
+cargo +nightly build --package dodge-the-creeps --target wasm32-unknown-emscripten -Zbuild-std $@ &&
+if [ -n "$GODOT4_BIN" ] ; then
+    case $* in
+        *--release* )
+            echo Godot export release
+            $GODOT4_BIN --headless --path ../godot --export-release Web
+            ;;
+        * )
+            echo Godot export debug
+            $GODOT4_BIN --headless --path ../godot --export-debug Web
+            ;;
+    esac
+else
+    echo ; echo No variable GODOT4_BIN found, skipping export.
+fi
